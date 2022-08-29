3fc7181f3c949f1fa88774e465284e99a01939fc
Ahmed Hassan <ahmed.hassan@Aru.edu.eg>
2022-08-29 04:29:01 +0200

    add the background color

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,7 @@ import Form from './Form';
 const Login = () => {
     return (
         <div className={classes.container}>
-           <div className='container'>
+           <div className={`container ${classes.wrapper}`}>
                 <div className={classes.logo}>
                     Logo
                 </div>
